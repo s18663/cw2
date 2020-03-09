f826c1ff8e5c1afbc8715d95502c3c20c4bfde3f
Zuzanna Bubrowska <s18663@pjwstk.edu.pl>
2020-03-09 11:45:05 +0100

    to cont

---
+++ cw2/MyComp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2
+{
+    public class MyComp : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            return StringComparer.InvariantCultureIgnoreCase.Equals($"{x.FirstName} {x.LastName} {x.Index}",
+            $"{y.FirstName} {y.LastName} {y.Index}");
+        }
+
+        public int GetHashCode(Student obj)
+        {
+           return StringComparer.CurrentCultureIgnoreCase.GetHashCode($"{obj.FirstName} {obj.LastName} {obj.Index}");
+        }
+    }
+}
---
+++ cw2/Studies.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace cw2
+{
+    public class Studies
+    {
+        public string Name { get; set; }
+        public string Mode { get; set; }
+
+    }
+}
